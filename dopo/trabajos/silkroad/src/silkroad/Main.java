6751d15ca0a6b15e3fd7416ddfd1a1c5a0ef1471
melancolia20i <melancolia20x@gmail.com>
2025-09-07 10:28:44 -0500

    OK

--- dopo/trabajos/silkroad/src/silkroad/Main.java
+++ dopo/trabajos/silkroad/src/silkroad/Main.java
@@ -3,13 +3,22 @@ import shapes.*;
 
 public class Main
 {
+	/* Este es el numero maximo que se puede ingresar para crear
+	 * una carretera. Es 17 dado que la ventana es 500x500 y cada
+	 * road chunk es de 100x100
+	 * */
+	private static final int maxRoadLength = 17;
+	
+	private static int roadLength;
+	
 	public static void main (String []args)
 	{
-		new Circle(0, 0, 100, shapes.ColorType.ROAD, 0, 0).makeVisible(true);
+		roadLength = 4;
 	}
 	
 	public static void getKeyPressed (char key)
 	{
 		
 	}
+	
 }
